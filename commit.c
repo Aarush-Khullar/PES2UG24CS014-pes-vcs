21b33abc69c876497eaf0b1de2ea40c4e4993bca
Aarush-Khullar <aarushkhullar1508@gmail.com>
2026-04-17 13:03:18 +0530

    Phase 4 - commit.c: Complete commit_create with serialization, object writing, and head update

--- commit.c
+++ commit.c
@@ -209,7 +209,14 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     commit.timestamp = (uint64_t)time(NULL);
     snprintf(commit.message, sizeof(commit.message), "%s", message ? message : "");
 
-    // TODO: Implement rest
-    (void)commit_id_out;
-    return -1;
+    void *data = NULL;
+    size_t len = 0;
+    if (commit_serialize(&commit, &data, &len) != 0) return -1;
+
+    int rc = object_write(OBJ_COMMIT, data, len, commit_id_out);
+    free(data);
+    if (rc != 0) return -1;
+
+    return head_update(commit_id_out);
+}
 }
