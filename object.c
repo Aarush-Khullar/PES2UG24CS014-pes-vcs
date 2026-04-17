9d521749fb3f9086a1e00ac5ecfce6ac9737a6be
Aarush-Khullar <aarushkhullar1508@gmail.com>
2026-04-17 12:58:06 +0530

    Phase 1 - object.c: Add hash computation and deduplication check for object_write

--- object.c
+++ object.c
@@ -113,9 +113,14 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
     memcpy(object, header, (size_t)header_len);
     if (len > 0) memcpy(object + header_len, data, len);
 
+    compute_hash(object, total_len, id_out);
+    if (object_exists(id_out)) {
+        free(object);
+        return 0;
+    }
+
     // TODO: Implement rest
     free(object);
-    (void)id_out;
     return -1;
 }
 
