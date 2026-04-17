f6012a1ceb3ba38984add3e6ccf214c43e1b2c4f
Aarush-Khullar <aarushkhullar1508@gmail.com>
2026-04-17 13:03:07 +0530

    Phase 4 - commit.c: Add author, timestamp, and message setting in commit_create

--- commit.c
+++ commit.c
@@ -205,7 +205,11 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
         commit.has_parent = 0;
     }
 
+    snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
+    commit.timestamp = (uint64_t)time(NULL);
+    snprintf(commit.message, sizeof(commit.message), "%s", message ? message : "");
+
     // TODO: Implement rest
-    (void)message; (void)commit_id_out;
+    (void)commit_id_out;
     return -1;
 }
